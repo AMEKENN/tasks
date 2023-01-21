cdc9d4bad754e9909e90880709ccc23aa1875095
AMEKENN <40465993+AMEKENN@users.noreply.github.com>
2023-01-21 21:47:54 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+```
+#团圆领红包助力
+
+需要定义环境变量 krtycode 指定助力码
+
+务必先先获取到助力码后填写变量
+
+[task_local]
+#团圆领红包助力
+11 11 11 11 * jd_ty_help.js, tag=团圆领红包助力, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+```
