d513cfb6c1b8807d3fd85345803eed3eead24ed8
AMEKENN <40465993+AMEKENN@users.noreply.github.com>
2023-01-21 22:17:56 +0800

    Update task_test.py

--- task_test.py
+++ task_test.py
@@ -1,4 +1,4 @@
-```
+'''
 #团圆领红包助力
 
 需要定义环境变量 krtycode 指定助力码
@@ -8,6 +8,6 @@
 [task_local]
 #团圆领红包助力
 11 11 11 11 * jd_ty_help.js, tag=团圆领红包助力, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
-```
+'''
 if __name__ == "__main__":
     print("111111")
