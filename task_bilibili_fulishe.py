c7066fe586231edbea1ab44c7c793b243c72a7d1
AMEKENN <40465993+AMEKENN@users.noreply.github.com>
2023-12-05 16:24:24 +0800

    Update task_bilibili_fulishe.py

--- task_bilibili_fulishe.py
+++ task_bilibili_fulishe.py
@@ -13,11 +13,17 @@ TASK_NAME = '哔哩哔哩 福利社抢会员兑换'
 config = {
     'Ray_BiliBiliCookies__0': '',
     'Ray_Security__UserAgent': '',
-    'csrf': '',
     'TASKS_TG_USER_ID': ''
 }
+cookies = {}
 
 load_config(config)
+
+temp_list = config.get('Ray_BiliBiliCookies__0').split('; ')
+for data in temp_list:
+    key = data.split('=', 1)[0]
+    value = data.split('=', 1)[1]
+    cookies[key] = value
 
 headers = {
     'Host': 'app.bilibili.com',
@@ -31,13 +37,13 @@ headers = {
     'Sec-Fetch-Dest': 'empty',
     'Referer': 'https://www.bilibili.com/blackboard/activity-new-freedata.html?share_source=COPY&share_plat=android&share_tag=s_i&url_from_h5=1%2C1&plat_id=124&share_from=h5&share_medium=android&unique_k=CNuA4FT&timestamp=1674795097&native.theme=1',
     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Cookie': config.get('Ray_BiliBiliCookies__0'),
+    'Cookie': config.get('Ray_BiliBiliCookies__0')
 }
 
 data = {
     'cross_domain': 'true',
     'id': '3',
-    'csrf': config.get('csrf'),
+    'csrf': cookies.get('bili_jct')
 }
 
 response = requests.post('https://app.bilibili.com/x/wall/unicom/order/pack/receive', headers=headers, data=data)
